c25c5aa88c7538783453cb4b513cf8a59e002cfe
Janne Salokoski <janne.salokoski@helsinki.fi>
2025-07-17 22:37:16 +0300

    feat(db): add methods for connecting to the db

---
+++ backend/app/database.py
@@ -0,0 +1,9 @@
+from sqlmodel import SQLModel, create_engine, Session
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL, echo=True)
+
+
+def get_session():
+    return Session(engine)
