80bda3805ab05cbfa5b787ed9ce61ca56e4dc844
Janne Salokoski <janne.salokoski@helsinki.fi>
2025-07-17 14:11:06 +0300

    fix(routes): server api at /api

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,10 +2,10 @@ from fastapi import FastAPI
 from app.r2_utils import generate_presigned_upload_url
 from uuid import uuid4
 
-app = FastAPI()
+app = FastAPI(root_path="/api")
 
 
-@app.get("/api/upload-url")
+@app.get("/upload-url")
 def get_upload_url():
     object_key = f"uploads/{uuid4()}.jpg"
     url = generate_presigned_upload_url(object_key)
